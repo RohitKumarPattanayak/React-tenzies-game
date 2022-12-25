6d4c013278ec5343b3aeeabfc82fe367e198a382
RohitKumarPattanayak <rohitkumarpattanayak@gmail.com>
2022-12-26 00:06:38 +0530

    rolling random number functionality conpleted

---
+++ src/app.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Die from "./components/Die";
+
+export default function App() {
+  const [dice, setDice] = React.useState(allNewDice());
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6));
+    }
+    return newDice;
+  }
+
+  function rollDice() {
+    setDice(allNewDice());
+  }
+
+  const diceElements = dice.map((die) => <Die value={die} />);
+
+  return (
+    <center>
+      <main>
+        <div className="dice-container">{diceElements}</div>
+        <button className="roll-dice" onClick={rollDice}>
+          Roll
+        </button>
+      </main>
+    </center>
+  );
+}
